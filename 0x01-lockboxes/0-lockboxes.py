f7c5a03399d86488c6add51bf01ad371183a0e5b
Olatunde, J. Oloyede <olatundeoloyede41@yahoo.com>
2023-08-08 03:20:10 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,7 +7,6 @@ This module defines a function to determine if all the boxes can be opened based
 
 
 def canUnlockAll(boxes):
-  
     """
     Determine if all boxes can be opened based on provided keys.
 
@@ -16,21 +15,10 @@ def canUnlockAll(boxes):
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
-
-    Example:
-        >>> boxes1 = [[1], [2], [3], []]
-        >>> canUnlockAll(boxes1)
-        True
-
-        >>> boxes2 = [[1, 3], [3, 0, 1], [2], [0]]
-        >>> canUnlockAll(boxes2)
-        False
     """
     # Create a set to keep track of opened boxes
     opened_boxes = set()
-    
-    # Start with the first box (box 0) opened
-    opened_boxes.add(0)
+    opened_boxes.add(0)  # Start with the first box (box 0) opened
     
     # Create a queue for BFS traversal
     queue = [0]
@@ -52,8 +40,17 @@ def canUnlockAll(boxes):
 boxes1 = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes1))  # Output: True
 
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+boxes2 = [[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]]
 print(canUnlockAll(boxes2))  # Output: True
 
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+boxes3 = [[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes3))  # Output: False
+
+boxes4 = [[0]]
+print(canUnlockAll(boxes4))  # Output: True
+
+boxes5 = [[10, 3, 8, 9, 6, 5, 8, 1], [8, 5, 3, 7, 1, 8, 6], [5, 1, 9, 1], [], [6, 6, 9, 4, 3, 2, 3, 8, 5], [9, 4], [4, 2, 5, 1, 1, 6, 4, 5, 6], [9, 5, 8, 8], [6, 2, 8, 6]]
+print(canUnlockAll(boxes5))  # Output: True
+
+boxes6 = [[7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3], [7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7], [4, 2, 9, 6, 6, 5, 5]]
+print(canUnlockAll(boxes6))  # Output: False
