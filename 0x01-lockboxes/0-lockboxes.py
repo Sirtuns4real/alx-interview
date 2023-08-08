6c8e78cbf825e3cfb7daeabd7a99caabbc5b3091
Olatunde, J. Oloyede <olatundeoloyede41@yahoo.com>
2023-08-08 03:17:37 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -49,8 +49,11 @@ def canUnlockAll(boxes):
     return len(opened_boxes) == len(boxes)
 
 # Test cases
-boxes1 = [[1], [2], [3], []]
+boxes1 = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes1))  # Output: True
 
-boxes2 = [[1, 3], [3, 0, 1], [2], [0]]
-print(canUnlockAll(boxes2))  # Output: False
+boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes2))  # Output: True
+
+boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes3))  # Output: False
